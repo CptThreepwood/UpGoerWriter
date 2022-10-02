cb28880a6365fa21a004fe956de4e07583cb8239
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 23:10:00 +0000

    Adding tarfile member sanitization to extractall()

--- translators/SpacyTranslator/load_model.py
+++ translators/SpacyTranslator/load_model.py
@@ -25,7 +25,26 @@ def download_model_from_s3(model_name: str, local_path: str) -> str:
         s3.download_file(S3_BUCKET, s3_prefix, local_compressed)
 
     with tarfile.open(local_compressed) as f:
-        f.extractall(path=local_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(f, path=local_path)
 
     return local_path
 
