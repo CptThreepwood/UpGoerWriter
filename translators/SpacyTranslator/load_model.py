7d583af3ae02c0e0bc6f6304269d83543a5a854f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 20:41:21 +0000

    Adding numeric_owner as keyword arguement

--- translators/SpacyTranslator/load_model.py
+++ translators/SpacyTranslator/load_model.py
@@ -41,7 +41,7 @@ def download_model_from_s3(model_name: str, local_path: str) -> str:
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(f, path=local_path)
